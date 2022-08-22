54e9d4ace24dfa845539a7b19e8f81abf8689e0b
nurg1ssa <nurike2001@mail.ru>
2022-08-22 13:24:13 +0600

    footer added

---
+++ src/components/pages/Services.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../../App.css'
+
+export default function Services() {
+  return (
+    <div className='services'>Services</div>
+  )
+}
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import '../../App.css'
+export default function SignUp() {
+  return (
+    <div className='sign-up'>Sign Up</div>
+  )
+}
